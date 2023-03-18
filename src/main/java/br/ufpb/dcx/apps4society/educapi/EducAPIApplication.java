2168f5aa51be6de2113f9b1f6a3be9728dc2e11d
Robinson Mourão <bob.info.guaratiba@gmail.com>
2023-03-18 06:47:57 -0300

    Add an user in EducAPIApplication, Set test profile and H2 by default.

--- src/main/java/br/ufpb/dcx/apps4society/educapi/EducAPIApplication.java
+++ src/main/java/br/ufpb/dcx/apps4society/educapi/EducAPIApplication.java
@@ -1,11 +1,20 @@
 package br.ufpb.dcx.apps4society.educapi;
 
+import br.ufpb.dcx.apps4society.educapi.domain.Context;
+import br.ufpb.dcx.apps4society.educapi.domain.User;
+import br.ufpb.dcx.apps4society.educapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Controller
 @SpringBootApplication
@@ -13,6 +22,9 @@ public class EducAPIApplication {
 
 	@Value("${app.version}")
 	private String version;
+
+	@Autowired
+	UserRepository userRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EducAPIApplication.class, args);	
@@ -23,5 +35,28 @@ public class EducAPIApplication {
 	public String index() {
       return String.format("Welcome to EducAPI! | VERSION: v%s", this.version);
     }
+
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+
+			User admin = new User();
+			admin.setEmail("admin@educapi.com");
+			admin.setPassword("12345678");
+			admin.setName("Administrador");
+			admin.setId(1L);
+			this.userRepository.save(admin);
+
+			Context context = new Context();
+			context.setId(1L);
+			Set<Context> contextSet = new HashSet<>();
+			contextSet.add(context);
+			admin.setContexts(contextSet);
+
+
+		};
+
+	}
 
 }
