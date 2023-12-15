0f1441a08591bcdc58bd2dc0e3aeacb2fe4b8e7a
RonyAbreu <ronyelison78@gmail.com>
2023-12-15 15:43:02 -0300

    Correction: correção dos imports do java-jwt e dos métodos para gerar o token e recuperar o subject do token

--- src/main/java/br/ufpb/dcx/apps4society/educapi/services/JWTService.java
+++ src/main/java/br/ufpb/dcx/apps4society/educapi/services/JWTService.java
@@ -1,21 +1,27 @@
 package br.ufpb.dcx.apps4society.educapi.services;
 
+import java.util.Date;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.AlgorithmMismatchException;
+import com.auth0.jwt.exceptions.MissingClaimException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
 import br.ufpb.dcx.apps4society.educapi.domain.User;
 import br.ufpb.dcx.apps4society.educapi.dto.user.UserLoginDTO;
 import br.ufpb.dcx.apps4society.educapi.filter.TokenFilter;
 import br.ufpb.dcx.apps4society.educapi.repositories.UserRepository;
 import br.ufpb.dcx.apps4society.educapi.response.LoginResponse;
 import br.ufpb.dcx.apps4society.educapi.services.exceptions.InvalidUserException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.SignatureException;
-import org.apache.commons.validator.routines.EmailValidator;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-import java.util.Date;
-import java.util.Optional;
 
 @Service
 public class JWTService {
@@ -23,60 +29,60 @@ public class JWTService {
     @Autowired
     private UserRepository userRepository;
 
-    @Value("${app.token.key}")
+    @Value("${app.token.key:token_key}")
     private String TOKEN_KEY;
 
     public JWTService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-    
+
     public LoginResponse authenticate(UserLoginDTO userLoginDTO) throws InvalidUserException {
 
-        Optional<User> userOptional = userRepository.findByEmailAndPassword(userLoginDTO.getEmail(), userLoginDTO.getPassword());
-        if (userOptional.isEmpty()){
+        Optional<User> userOptional = userRepository.findByEmailAndPassword(userLoginDTO.getEmail(),
+                userLoginDTO.getPassword());
+        if (userOptional.isEmpty()) {
             throw new InvalidUserException();
         }
 
         return new LoginResponse(generateToken(userLoginDTO));
     }
 
-    private String generateToken(UserLoginDTO userLoginDTO){
-        return Jwts.builder()
-                .setSubject(userLoginDTO.getEmail())
-                .signWith(SignatureAlgorithm.HS512, TOKEN_KEY)
-                .setExpiration(new Date(System.currentTimeMillis() + 30 * 60 * 1000)).compact();
+    private String generateToken(UserLoginDTO userLoginDTO) {
+        Algorithm algorithm = Algorithm.HMAC256(TOKEN_KEY.getBytes());
+        return JWT.create()
+                .withSubject(userLoginDTO.getEmail())
+                .withExpiresAt(expiration())
+                .sign(algorithm)
+                .strip();
 
     }
 
-    public Optional<String> recoverUser(String header){
-        if (header == null || !header.startsWith("Bearer ")){
-            throw new SecurityException();
+    private Date expiration() {
+        return new Date(System.currentTimeMillis() + 30 * 60 * 1000);
+    }
+
+    public String recoverUser(String header) {
+        if (header == null || !header.startsWith("Bearer ")) {
+            throw new SecurityException("Token invalid or expired!");
         }
 
         String token = header.substring(TokenFilter.TOKEN_INDEX);
         String subject;
 
         try {
-            subject = Jwts.parser().setSigningKey(TOKEN_KEY).parseClaimsJws(token).getBody().getSubject();
-            if(!emailValidator(subject)){
-                return Optional.empty();
-            }
-        }catch (SignatureException error){
-           throw new SecurityException("Token invalid or expired!");
-        }        
+            Algorithm algorithm = Algorithm.HMAC256(TOKEN_KEY.getBytes());
+            JWTVerifier verifier = JWT.require(algorithm).build();
+            DecodedJWT decodedJWT = verifier.verify(token);
+            subject = decodedJWT.getSubject();
+        } catch (TokenExpiredException | MissingClaimException
+                | SignatureVerificationException | AlgorithmMismatchException | IllegalArgumentException error) {
+            throw new SecurityException("Token invalid or expired!");
+        }
 
-        return Optional.of(subject);
-    }    
-
-    public String tokenBearerFormat(String token){
-        String bearedToken = "Bearer " + token;
-        return bearedToken;
+        return subject;
     }
 
-    public boolean emailValidator(String email){
-        boolean valid = EmailValidator.getInstance().isValid(email);
-        return valid;
+    public String tokenBearerFormat(String token) {
+        return "Bearer " + token;
     }
 }
-
-
