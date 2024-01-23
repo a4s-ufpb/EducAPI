05191f86d4ff7b54d51e2116dc9cad4510f49984
RonyAbreu <ronyelison78@gmail.com>
2024-01-23 08:22:44 -0300

    Refactor: Refatorando código do Handler de Exceções da API

--- src/main/java/br/ufpb/dcx/apps4society/educapi/resources/exceptions/StandardError.java
+++ src/main/java/br/ufpb/dcx/apps4society/educapi/resources/exceptions/StandardError.java
@@ -7,12 +7,21 @@ public class StandardError implements Serializable{
 	private Integer status;
 	private String msg;
 	private Long timeStamp;
-	
-	public StandardError(Integer status, String msg, Long timeStamp) {
-		super();
+	private String path;
+
+	public StandardError(Integer status, String msg, Long timeStamp, String path) {
 		this.status = status;
 		this.msg = msg;
 		this.timeStamp = timeStamp;
+		this.path = path;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
 	}
 
 	public Integer getStatus() {
